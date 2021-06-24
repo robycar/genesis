30d6e797adb26cee7ff66ed6b6b4def3a992377f
marco0167 <marcocoppola01671@gmail.com>
2021-06-24 09:12:33 +0200

    add report

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -15,13 +15,15 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Link from "@material-ui/core/Link";
+import Navbar from "../components/Navbar";
+import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import { mainListItems, secondaryListItems } from "../components/listItems";
 import TotalTestSuite from "../components/TotalTestSuite";
 import Deposits from "../components/Deposits";
-import Orders from "../components/Orders";
+import Orders from "../components/TestCaseComplete";
 
 function Copyright() {
   return (
@@ -115,6 +117,30 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     height: 240,
   },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginTop: "10px",
+    marginBottom: "10px",
+  },
+  buttonNotClickedBlue: {
+    backgroundColor: "whute",
+    border: "1px solid #1665D8",
+    variant: "contained",
+    color: "#1665D8",
+    marginLeft: "10px",
+    marginRight: "10px",
+    width: "200px",
+    height: "40px",
+  },
+  buttonClickedBlue: {
+    backgroundColor: "#1665D8",
+    color: "primary",
+    marginLeft: "10px",
+    marginRight: "10px",
+    width: "200px",
+    height: "40px",
+  },
 }));
 
 function Report() {
@@ -135,34 +161,7 @@ function Report() {
         position="absolute"
         className={clsx(classes.appBar, open && classes.appBarShift)}
       >
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(
-              classes.menuButton,
-              open && classes.menuButtonHidden
-            )}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            Dashboard
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-        </Toolbar>
+        <Navbar />
       </AppBar>
       <Drawer
         variant="permanent"
@@ -184,20 +183,23 @@ function Report() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* TotalTestSuite */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                <TotalTestSuite />
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
+          <Grid container spacing={3}>            
             {/* Recent Orders */}
+            <Grid item xs={12}>
+              <div className={classes.buttonContainer}>
+                <Button className={classes.buttonNotClickedBlue}>
+                  Test Schedulati
+                </Button>
+
+                <Button
+                  className={classes.buttonClickedBlue}
+                  variant="contained"
+                  color="primary"
+                >
+                  Test Conclusi
+                </Button>
+              </div>
+            </Grid>
             <Grid item xs={12}>
               <Paper className={classes.paper}>
                 <Orders />
