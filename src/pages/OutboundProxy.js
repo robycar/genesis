e29a6d502dbb576f75b0f4e8496030d27b5071e6
marco0167 <marcocoppola01671@gmail.com>
2021-06-29 15:43:34 +0200

    fix bug

---
+++ src/pages/OutboundProxy.js
@@ -0,0 +1,256 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Navbar from "../components/Navbar";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Container from "@material-ui/core/Container";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { mainListItems, secondaryListItems } from "../components/listItems";
+import NavbarItemEdit from "../components/NavbarItemEdit";
+import ButtonClickedGreen from "../components/ButtonClickedGreen";
+import ButtonNotClickedGreen from "../components/ButtonNotClickedGreen";
+import { FormControl, Paper, Typography } from "@material-ui/core";
+import SelectBar from "../components/SelectBar";
+import InputBase from "@material-ui/core/InputBase";
+import DnsIcon from "@material-ui/icons/Dns";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InputLabel from "@material-ui/core/InputLabel";
+
+const drawerWidth = 240;
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    "label + &": {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    width: "300px",
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: "none",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+    //backgroundColor: "yellow",
+    alignItems: "center",
+  },
+  fixedHeight: {
+    height: 240,
+  },
+  buttonContainer: {
+    marginBottom: "20px",
+  },
+  generalContainer: {
+    display: "flex",
+    marginTop: "5%",
+    backgroundColor: "yellow",
+  },
+  icon: {
+    color: "rgba(71, 184, 129, 1)",
+  },
+  titolo: {
+    fontWeight: 500,
+    fontStyle: "normal",
+    fontSize: "24px",
+    color: "#66788A",
+    lineHeight: "20px",
+    color: "rgba(71, 184, 129, 1)",
+  },
+  bootstrapInput: {
+    flexDirection: "row",
+    width: "400px",
+  },
+  label: {
+    padding: "5%",
+    width: "200px",
+  },
+  divLabel: {
+    marginTop: "1%",
+  },
+}));
+
+function OutboundProxy() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Navbar />
+      </AppBar>
+
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+
+        <Container maxWidth="lg" className={classes.container}>
+          <div className={classes.containerNavbarItem}>
+            <NavbarItemEdit fontSize="large" />
+          </div>
+        </Container>
+        <div className={classes.buttonContainer}>
+          <ButtonNotClickedGreen nome="LINEE" />
+
+          <ButtonClickedGreen nome="OUTBOUND PROXY" />
+
+          <ButtonNotClickedGreen nome="TEMPLATE" />
+
+          <ButtonNotClickedGreen nome="TEST" />
+        </div>
+
+        <Paper className={classes.paper}>
+          <ListItem>
+            <ListItemIcon className={classes.listItemIcon}>
+              <DnsIcon fontSize="large" className={classes.icon} />
+            </ListItemIcon>
+            <Typography className={classes.titolo}> Outbound Proxy </Typography>
+          </ListItem>
+
+          <Container className={classes.generalContainer}>
+            <Paper className={classes.divLabel}>
+              <Typography className={classes.label}>
+                Proxy IP Address:{" "}
+              </Typography>{" "}
+            </Paper>
+            <Paper className={classes.bootstrapInput}>
+              <FormControl className={classes.margin}>
+                <BootstrapInput id="demo-customized-textbox" />
+              </FormControl>
+            </Paper>
+          </Container>
+        </Paper>
+      </main>
+    </div>
+  );
+}
+
+export default OutboundProxy;
