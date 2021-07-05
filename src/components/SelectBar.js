bea7443020fe79c40795cab3a2a075c8b24a9ea0
Guy Roger Eboulet <g.eboulet@be-tse.it>
2021-07-05 17:18:51 +0200

    Modale testCase

--- src/components/SelectBar.js
+++ src/components/SelectBar.js
@@ -4,10 +4,12 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
+import "../styles/App.css";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(2),
+    // width: "20vw",
     width: "380px",
   },
   select: {
@@ -16,19 +18,39 @@ const useStyles = makeStyles((theme) => ({
     padding: "2%",
     alignItems: "center",
   },
+
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+
+  inputLabel: {
+    fontSize: "2vh",
+    alignSelf: "center",
+  },
 }));
 
 function SelectBar(props) {
   const classes = useStyles();
   const [test, setTest] = React.useState("");
+
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
   const handleChange = (event) => {
     setTest(event.target.value);
   };
+
   return (
     <FormControl variant="outlined" className={classes.formControl}>
-      <InputLabel id="demo-simple-select-outlined-label">
-        {" "}
-        {props.nome}{" "}
+      <InputLabel
+        className={classes.inputLabel}
+        ref={inputLabel}
+        id="demo-simple-select-outlined-label"
+      >
+        {props.nome}
       </InputLabel>
       <Select
         className={classes.select}
@@ -36,12 +58,13 @@ function SelectBar(props) {
         id="demo-simple-select-outlined"
         value={test}
         onChange={handleChange}
-        label="Test"
+        labelWidth={labelWidth}
+        // label="Age"
       >
         <MenuItem value="">
           <em>None</em>
         </MenuItem>
-        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={10}>Ciao</MenuItem>
         <MenuItem value={20}>Twenty</MenuItem>
         <MenuItem value={30}>Thirty</MenuItem>
       </Select>
