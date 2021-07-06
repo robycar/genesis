fb6b4d5b87167427446bcbf6afa69cd0d301c56c
Guy Roger Eboulet <g.eboulet@be-tse.it>
2021-07-06 10:55:18 +0200

    Okay Launching

--- src/pages/Launching.js
+++ src/pages/Launching.js
@@ -104,8 +104,10 @@ const useStyles = makeStyles((theme) => ({
   },
   generalContainer: {
     display: "flex",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     padding: "20px",
+    // backgroundColor: "red",
+    width: "100%",
   },
   paperTest: {
     border: "1px",
@@ -119,13 +121,13 @@ const useStyles = makeStyles((theme) => ({
   },
   paperContainer2: {
     flexGrow: "4",
-    flexDirection: "row",
+    flexDirection: "column",
     padding: "20px",
     marginBottom: "20px",
+    marginLeft: "20px",
   },
   scritta: {
     padding: "20px",
-    // fontFamily: "roboto",
     fontSize: "26px",
     color: "#47B881",
     letterSpacing: "0.15px",
@@ -143,8 +145,11 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "15px",
   },
   bottone: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
     marginTop: "15px",
-    marginLeft: "620px",
+    // marginLeft: "62px",
   },
 }));
 
@@ -191,7 +196,7 @@ function Launching() {
             <NavbarItemLaunch />
           </div>
 
-          <Paper className={classes.generalContainer} elevation={1}>
+          <div className={classes.generalContainer} elevation={1}>
             <Paper
               className={classes.paperContainer1}
               variant="outlined"
@@ -221,12 +226,14 @@ function Launching() {
                   Informazioni{" "}
                 </Typography>
                 <Typography className={classes.testo}>
-                  Numero Chiamante = 01225
-                  <br /> Tipo Linea Chiamante = FIBRA <br /> Numero Chiamato
-                  <br /> Tipo Line Chiamato
-                  <br /> Numero Chiamante 2
-                  <br /> Tipo Linea Chiamante 2
-                  <br /> Numero Chiamante 3
+                  Numero Chiamante = 01225*****
+                  <br /> Tipo Linea Chiamante = FIBRA <br /> Numero Chiamato =
+                  2494******
+                  <br /> Tipo Linea Chiamante = FIBRA
+                  <br /> Numero Chiamante 2 = 0126*****
+                  <br /> Tipo Linea Chiamante 2 = *****
+                  <br /> Numero Chiamante 3 = 0127*****
+                  <br /> Tipo Linea Chiamante 3 = *****
                 </Typography>
               </Paper>
 
@@ -252,7 +259,7 @@ function Launching() {
                 <ButtonClickedGreen nome="Start" />
               </div>
             </Paper>
-          </Paper>
+          </div>
 
           {/*          <Paper className={classes.paperTest}>
                 <Typography className={classes.scritta}>
