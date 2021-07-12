a0ae0b0bd24f02e226a2a637194078d5f5e5d61b
marco0167 <marcocoppola01671@gmail.com>
2021-07-12 13:13:32 +0200

    change header Typography

--- src/pages/amministrazione/AmministrazioneUtenze.js
+++ src/pages/amministrazione/AmministrazioneUtenze.js
@@ -11,15 +11,13 @@ import IconButton from "@material-ui/core/IconButton";
 import Container from "@material-ui/core/Container";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { mainListItems, secondaryListItems } from "../../components/listItems";
-import ButtonClickedGreen from "../../components/ButtonClickedGreen";
-import ButtonNotClickedGreen from "../../components/ButtonNotClickedGreen";
-import { Paper } from "@material-ui/core";
 import GestioneUtenti from "../../components/GestioneUtenti";
 import NavbarItemAdmin from "../../components/NavbarItemAdmin";
 import { NavLink } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import "../../styles/App.css";
-import Typography from "@material-ui/core/Typography";
+import { ListItem, ListItemIcon, Paper, Typography } from "@material-ui/core";
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 const drawerWidth = 240;
 
@@ -102,6 +100,27 @@ const useStyles = makeStyles((theme) => ({
   },
   buttonContainer: {
     marginBottom: "20px",
+  },
+  icon: {
+    color: "rgba(71, 184, 129, 1)",
+  },
+  titolo: {
+    fontWeight: 500,
+    fontStyle: "normal",
+    fontSize: "24px",
+    color: "#66788A",
+    lineHeight: "20px",
+    color: "rgba(71, 184, 129, 1)",
+    padding: "2%",
+    // marginTop: "2%",
+  },
+  divider: {
+    width: "90%",
+    marginLeft: "5%",
+    lineHeight: "1px",
+  },
+  generalPaper: {
+    alignItems: "baseline",
   },
 }));
 
@@ -189,7 +208,14 @@ function AmministrazioneUtenze() {
               GRUPPO
             </Button>
           </div>
-          <Paper className={classes.paper}>
+          <Paper className={classes.generalPaper}>
+            <ListItem>
+              <ListItemIcon>
+                <AccountBoxIcon fontSize="large" className={classes.icon} />
+              </ListItemIcon>
+              <Typography className={classes.titolo}> Utenze </Typography>
+            </ListItem>
+            <Divider className={classes.divider} />
             <GestioneUtenti />
           </Paper>
         </main>
