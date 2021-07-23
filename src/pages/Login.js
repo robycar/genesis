e139312f4bb27cc805c56d67a58a3d6d1d516f19
softinside <p.taviani@reply.it>
2021-07-23 09:14:26 +0200

    Login Ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,121 @@
+// import React, { useState, useEffect } from "react";
+// import { useHistory } from "react-router";
+
+// const Login = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const history = useHistory();
+//   useEffect(() => {
+//     if (localStorage.getItem("user-info")) {
+//       // history.push("/dashboard/testcase");
+//     }
+//   }, []);
+//   async function login() {
+//     console.warn(username, password);
+//     var myHeaders = new Headers();
+//     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+//     var urlencoded = new URLSearchParams();
+//     urlencoded.append("username", username);
+//     urlencoded.append("password", password);
+
+//     var requestOptions = {
+//       method: "POST",
+//       headers: myHeaders,
+//       body: urlencoded,
+//       redirect: "follow",
+//     };
+//     let result = await fetch(
+//       `http://localhost:9081/api/auth/login`,
+//       requestOptions
+//     );
+
+//     result = await result.json();
+//     console.log(result);
+//     localStorage.setItem("token", JSON.stringify(result.access_token));
+//     history.push("/dashboard/testcase");
+//   }
+
+//   return (
+//     <div>
+//       <h1>Login Page</h1>
+//       <div className="col-sm-6 offset-sm-3">
+//         <input
+//           type="text"
+//           placeholder="user"
+//           className="form-control"
+//           onChange={(e) => setUsername(e.target.value)}
+//         />
+//         <br />
+//         <input
+//           type="text"
+//           placeholder="password"
+//           className="form-control"
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <br />
+//         <button className="btn btn-primary" onClick={login}>
+//           Login
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Login = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+  const classes = useStyles();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -38,29 +152,58 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <div className="col-sm-6 offset-sm-3">
-        <input
-          type="text"
-          placeholder="user"
-          className="form-control"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="password"
-          className="form-control"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button className="btn btn-primary" onClick={login}>
-          Login
-        </button>
-      </div>
-    </div>
-  );
-};
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
 
-export default Login;
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="user"
+            autoComplete="off"
+            autoFocus
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            // className={classes.submit}
+            onClick={login}
+          >
+            Accedi
+          </Button>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
