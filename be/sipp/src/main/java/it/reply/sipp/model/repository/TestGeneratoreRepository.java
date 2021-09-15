6e70d5536541eb348be3e04a53634c7a93b1e5b7
Roberto Carta <r.carta@reply.it>
2021-09-15 16:33:14 +0200

    BE: Fix Errore compilazione

--- be/sipp/src/main/java/it/reply/sipp/model/repository/TestGeneratoreRepository.java
+++ be/sipp/src/main/java/it/reply/sipp/model/repository/TestGeneratoreRepository.java
@@ -4,10 +4,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import it.reply.sipp.model.LineaGeneratoreVO;
 import it.reply.sipp.model.TestGeneratoreVO;
 
 public interface TestGeneratoreRepository extends JpaRepository<TestGeneratoreVO, Long> {
 
   Optional<TestGeneratoreVO> findByNome(String nome);
+
+  long countByLineaChiamanteOrLineaChiamato(LineaGeneratoreVO lineaChiamante, LineaGeneratoreVO lineaChiamato);
 
 }
