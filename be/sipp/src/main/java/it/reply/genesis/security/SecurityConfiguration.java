4fda514a402bcc1c892093ae2b3f475dd17d05cc
Roberto Carta <r.carta@reply.it>
2021-10-13 17:50:02 +0200

    BE disabilitazione cors

--- be/sipp/src/main/java/it/reply/genesis/security/SecurityConfiguration.java
+++ be/sipp/src/main/java/it/reply/genesis/security/SecurityConfiguration.java
@@ -21,6 +21,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity(debug = false)
@@ -111,7 +114,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	}
 
 	
-
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
+  }
 
 //	@Bean
 //	public AuthenticationSuccessHandler successHandler() {
